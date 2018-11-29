41982d2917d13f3f8a4649f8a552fa8f183123bf
Pattaya Eye-dee <pattaya.eyedee@mail.kmutt.ac.th>
2018-11-29 17:40:55 +0700

    ????

--- NewWorldShop/src/java/nws/Servlet/FinalCheckoutServlet.java
+++ NewWorldShop/src/java/nws/Servlet/FinalCheckoutServlet.java
@@ -7,6 +7,10 @@ package nws.Servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Resource;
@@ -27,10 +31,12 @@ import nws.JDA.Users;
  * @author ASUS
  */
 public class FinalCheckoutServlet extends HttpServlet {
-@PersistenceUnit(unitName = "NewWorldShopPU")
-EntityManagerFactory emf;
-@Resource
-UserTransaction utx;
+
+    @PersistenceUnit(unitName = "NewWorldShopPU")
+    EntityManagerFactory emf;
+    @Resource
+    UserTransaction utx;
+
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -50,73 +56,71 @@ UserTransaction utx;
         int result;
         Users user = (Users) session.getAttribute("user");
         UsersJpaController UsersCtrl = new UsersJpaController(utx, emf);
-        if (user ==null) {
+        if (user == null) {
             getServletContext().getRequestDispatcher("/LoginPage.jsp").forward(request, response);
         }
         if (user != null) {
             if (address != null) {
                 if (creditno.equals(user.getIdcard())) {
-                    result = user.getMoney()- Integer.valueOf(totalPrice);
+                    result = user.getMoney() - Integer.valueOf(totalPrice);
                     user.setMoney(result);
                     UsersCtrl.edit(user);
                     getServletContext().getRequestDispatcher("/Complete.jsp").forward(request, response);
                 }
-            } else {
+                else {
                 getServletContext().getRequestDispatcher("/Checkout.jsp").forward(request, response);
             }
-            
-        }
-        getServletContext().getRequestDispatcher("/Checkout.jsp").forward(request, response);
-    }
-        // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-        /**
-         * Handles the HTTP <code>GET</code> method.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         * @throws ServletException if a servlet-specific error occurs
-         * @throws IOException if an I/O error occurs
-         */
-        @Override
-        protected void doGet
-        (HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    try {
-        processRequest(request, response);
-    } catch (Exception ex) {
-        Logger.getLogger(FinalCheckoutServlet.class.getName()).log(Level.SEVERE, null, ex);
-    }
-        }
 
-        /**
-         * Handles the HTTP <code>POST</code> method.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         * @throws ServletException if a servlet-specific error occurs
-         * @throws IOException if an I/O error occurs
-         */
-        @Override
-        protected void doPost
-        (HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    try {
-        processRequest(request, response);
-    } catch (Exception ex) {
-        Logger.getLogger(FinalCheckoutServlet.class.getName()).log(Level.SEVERE, null, ex);
-    }
+            } 
+            getServletContext().getRequestDispatcher("/Checkout.jsp").forward(request, response);
         }
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
 
-        /**
-         * Returns a short description of the servlet.
-         *
-         * @return a String containing servlet description
-         */
-        @Override
-        public String getServletInfo
-        
-            () {
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(FinalCheckoutServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(FinalCheckoutServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
         return "Short description";
-        }// </editor-fold>
+    }// </editor-fold>
 
-    }
+}
